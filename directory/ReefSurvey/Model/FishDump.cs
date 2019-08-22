d17d6edbedab4f56610fc42510d04ab054eb3e35
Justin Hughes <justin.a.hughes24@outlook.com>
2019-08-22 10:40:19 -0400

    Added FishData and Coordinates

--- directory/ReefSurvey/Model/FishDump.cs
+++ directory/ReefSurvey/Model/FishDump.cs
@@ -10,4 +10,21 @@ namespace Model
         public string CommonName { get; set; }
         public int TrophicID { get; set; }
     }
+
+    public class FishData
+    {
+        public int FishID { get; set; }
+        public int FishCount {get; set;}
+        public int StructureID{get; set;}
+        public double FishLength{get; set;}
+        public int LocationID {get; set;}
+    }
+
+    public class Coordinates
+    {
+        public int LocationID {get; set;}
+        public double Latitude {get; set;}
+        public double Longitude {get; set;}
+        public int SurveyIndex {get; set;}
+    }
 }

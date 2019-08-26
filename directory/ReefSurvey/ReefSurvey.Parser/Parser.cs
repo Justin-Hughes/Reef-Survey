42ab7a54f0f1c273f46ed2479847bb2597f66aed
Justin Hughes <justin.a.hughes24@outlook.com>
2019-08-26 14:44:09 -0400

    Commit

---
+++ directory/ReefSurvey/ReefSurvey.Parser/Parser.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using Microsoft.VisualBasic.FileIO;
+using System.IO;
+using Model;
+
+namespace ReefSurvey.Parser
+{
+    public class Parser
+    {
+        public string[] csvColumn = new string[17];  // 0 Region, 1 SubRegion, 2 StudyArea, 3 SurveyYear, 4 BatchCode, 5 SurveyIndex, 6 SurveyDate, 7 Latitude,
+                                                     // 8 Longitude, 9 Management, 10 StructureType, 11 Family, 12 ScientificName, 13 CommonName, 14 Trophic, 15 FishLenth, 16 FishCount
+
+        public static string dir = @"C:\Users\justi\OneDrive\Desktop\ReefSurvey\external\survey\1-data";
+        string[] csvPaths = Directory.GetFiles(dir, "Fish Dump.csv", System.IO.SearchOption.AllDirectories);
+
+        public void CSVParse()
+        {
+            bool writing = true;
+            foreach (var x in csvPaths)
+            {
+                TextFieldParser writer = new TextFieldParser(x);
+                writer.SetDelimiters(x, ",");
+                writer.TrimWhiteSpace = true;
+                while (writing == true)
+                {
+                    csvColumn = writer.ReadFields();
+                    foreach (var y in csvColumn)
+                    {
+                        var fish = new Fish { ScientificName };
+                    }
+                }
+                writer.Close();
+            }
+        }
+    }
+}
--- directory/ReefSurvey/ReefSurvey/Program.cs
+++ directory/ReefSurvey/ReefSurvey/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using ReefSurvey;
 
 namespace ReefSurvey
 {
@@ -6,7 +7,6 @@ namespace ReefSurvey
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
         }
     }
 }
